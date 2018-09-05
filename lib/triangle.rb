c46a58cde8c5a7fb38e956ea97d63dbd4549f488
Robert Snyder <r.snyder49@gmail.com>
2018-09-05 22:15:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,7 @@ class Triangle
   
   
   def kind
-    
-    if (@side1 + @side2) > @side3  || (@side2 + @side3) > @side1 || (@side1 + @side3) > @side2
-      begin
-        raise TriangleError
-      end
-    end
+
 
 
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
@@ -25,7 +20,13 @@ class Triangle
     end
     
     
-
+    if @side1 == @side2 && @side1 == @side3
+      :equilateral 
+      elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles 
+    else 
+      :scalene 
+    end
   end 
   
   class TriangleError < StandardError
