5511b0cdd7b1fd76f01261b7c1f0d0184e526f45
Robert Snyder <r.snyder49@gmail.com>
2018-09-05 21:56:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,20 @@ class Triangle
       end 
     end 
     
+    if (@side1 + @side2) > @side3 
+      begin
+        raise TriangleError
+      end
+      elsif (@side2 + @side3) > @side1 
+        begin 
+          raise TriangleError
+        end 
+      elsif (@side1 + @side3) > @side2 
+        begin 
+          raise TriangleError
+        end 
+      end 
+    
     if @side1 == @side2 && @side1 == @side3
       :equilateral 
       elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
@@ -25,8 +39,6 @@ class Triangle
   end 
   
   class TriangleError < StandardError
-    def message 
-    end
   end 
   
 end
