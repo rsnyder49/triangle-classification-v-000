b3285e95eacb6403aef2df12e19c9ee7e865ec41
Robert Snyder <r.snyder49@gmail.com>
2018-09-05 21:51:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,8 @@ class Triangle
       begin 
         raise TriangleError
       end 
+    end 
+    
     if @side1 == @side2 && @side1 == @side3
       :equilateral 
       elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
