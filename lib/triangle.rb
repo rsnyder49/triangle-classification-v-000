09911c12fafbf9e12ec9434b451711eb6614398c
Robert Snyder <r.snyder49@gmail.com>
2018-09-05 21:37:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   
   def initialize(attributes)
-    attributes.each{|key, value| self.send("#{key}=", value)
+    attributes.each{ |key, value| self.send("#{key}=", value) }
   end
   
   def kind 
